9767156b35a4dd44cc56171aeaf91e4e5c3d5150
Deepakab17 <deepaknab17@gmail.com>
2026-03-04 03:45:02 +0530

    MoodSession and User Interection added

--- app/models.py
+++ app/models.py
@@ -1,28 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
 class Mood(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(blank=True)
-
     def __str__(self):
         return self.name
-
-
 class Song(models.Model):
     external_id = models.CharField(max_length=100, unique=True)
     title = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
-    mood = models.ManyToManyField(
-        Mood,
-        related_name='songs'
-    )
+
+    mood = models.ManyToManyField(Mood, related_name='songs')
+
+    is_available = models.BooleanField(default=True)
+    last_synced = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.title} - {self.artist}"
-
-
 class Memory(models.Model):
     user = models.ForeignKey(
         User,
@@ -38,5 +33,27 @@ class Memory(models.Model):
     dedicated_to = models.CharField(max_length=200, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-created_at']
     def __str__(self):
         return f"{self.user.username} - {self.song.title}"
+    
+class UserSongInteraction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    mood=models.ForeignKey(Mood,on_delete=models.CASCADE)
+    play_count = models.IntegerField(default=0)
+    liked = models.BooleanField(default=False)
+    skipped_count = models.IntegerField(default=0)
+    last_played = models.DateTimeField(null=True, blank=True)
+    class Meta:
+        unique_together = ('user', 'song','mood')
+class MoodSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    mood = models.ForeignKey(Mood, on_delete=models.CASCADE)
+
+    songs = models.ManyToManyField(Song)
+    generated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-generated_at']
